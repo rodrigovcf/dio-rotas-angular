b17b1e81ca00ea90dfdbd92a66cf347b151c152e
Rodrigo Valença <rodrigovalenca@MacBook-Pro-de-Rodrigo.local>
2023-02-15 16:23:59 -0300

    Firsts Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
 const routes: Routes = [
-
+  {path:'', component: TitleComponent, pathMatch:'full'},
+  {path:'portfolio', component: CardComponent}
 ];
 
 @NgModule({
